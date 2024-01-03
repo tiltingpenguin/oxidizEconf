fce73f73f13e5dafc1c35bdc5c0fdc0964519efe
nkrapp <nico.krapp@suse.com>
2024-01-03 15:04:01 +0100

    refactor config reading and find dropins

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-use config::*;
-use glob::glob;
+use config::{*, builder::DefaultState};
+use std::collections::HashMap;
 use std::path::PathBuf;
 use log;
 
-fn get_conf_dirs(name: &str) -> Vec<PathBuf>{
+fn get_conf_dirs(name: &str) -> Vec<PathBuf> {
     //let mut paths: Vec<PathBuf> = Vec::new();
     let etc_dir = PathBuf::from("/etc/");
     let etc_subdir = etc_dir.join(name);
@@ -16,7 +16,8 @@ fn get_conf_dirs(name: &str) -> Vec<PathBuf>{
     let usr_libdir = PathBuf::from("/usr/lib/");
     let usr_libsubdir = usr_libdir.join(name);
 
-    let paths = vec![etc_dir, 
+    vec![
+        etc_dir, 
         etc_subdir, 
         run_dir, 
         run_subdir, 
@@ -24,44 +25,56 @@ fn get_conf_dirs(name: &str) -> Vec<PathBuf>{
         usr_etcsubdir, 
         usr_sharedir, 
         usr_sharesubdir, 
-        usr_libdir, 
+        usr_libdir,
         usr_libsubdir
-    ];
-    paths
+    ]
 }
 
-fn find_conf(mut paths: Vec<PathBuf>, name: &str, suffix: &str) -> String {
-    for file in paths.iter_mut() {
-        file.push(name);
-        file.set_extension(suffix);
-        let strpath = match file.to_str() {
-            Some(str) => str,
-            None => "",
-        };
-        for entry in glob(strpath).expect("Failed to read glob pattern") {
-            match entry {
-                Ok(path) => {
-                    log::info!("Highest priority config file: {}", path.display());
-                    return path.to_str().unwrap().to_string();
-                },
-                Err(e) => log::debug!("{}", e),
-            }
+
+fn find_dropins(conf_dirs: Vec<PathBuf>, name: &str/*, suffix: &str*/) -> Vec<PathBuf> {
+    let mut dropin_paths: Vec<PathBuf> = vec![];
+    for path in &conf_dirs {
+        let base = path.join(name);
+        let d = base.join(".d");
+        let confd = base.join(".conf.d");
+        if d.is_dir() {
+            dropin_paths.push(d);
+        } else if confd.is_dir() {
+            dropin_paths.push(confd);
         }
-        log::info!("{:?}: file not found", file)
     }
-    return "No file found".to_string();
+
+    dropin_paths
 }
 
-pub fn read_config(name: &str, suffix: &str, _format: FileFormat) -> Result<Config, ConfigError> {
+fn find_conf(mut paths: Vec<PathBuf>, name: &str, suffix: &str) -> Option<PathBuf> {
+    for path in paths.iter_mut() {
+        path.set_file_name(name);
+        path.set_extension(suffix);
+        if path.is_file() {
+            let p = path.clone();
+            return Some(p);
+        }
+    }
+    log::info!("No main config file found, reading dropins");
+    return None;
+}
+
+pub fn read_config(name: &str, suffix: &str, format: FileFormat) -> Result<Config, ConfigError> {
     let paths = get_conf_dirs(name);
+    let dropin_paths = paths.clone();
 
     let configfile = find_conf(paths, name, suffix);
+    let dropins = find_dropins(dropin_paths, name);
 
-    let settings = Config::builder()
-    .add_source(File::with_name(&configfile))
-    .build();
+    let mut builder: ConfigBuilder<DefaultState> = Config::builder();
 
-    return settings;
+    if configfile.is_some() {
+        let s = File::new(configfile.unwrap().to_str().unwrap(), format);
+        builder = builder.add_source(s);
+    }
+
+    builder.build()
 }
 /*
 pub fn new_config(format: Config::FileFormat) {
@@ -72,7 +85,7 @@ pub fn merge_config(file1: Config::Config, file2: Config::Config) {
 
 }
 
-pub fn write_file(config: Config::Config) {
+pub fn write_file(config: Config::Config, format: Config::FileFormat, path: PathBuf) -> Result<(), Error>{
 
 }
 
@@ -89,6 +102,10 @@ pub fn set_dirs(dirs: &[&str]) -> Config::ConfigError {
 }
 
 pub fn add_file_format(name: &str, delim: &str, comment: &str) -> Config::ConfigError{
+
+}
+
+pub fn set_defaults(defaults: HashMap) {
 
 }
 
