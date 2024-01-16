5cd9c07e279e30c3e08bda4e30c2dd7d4f750115
nkrapp <nico.krapp@suse.com>
2024-01-16 19:09:23 +0100

    Refactor to use builder pattern

--- src/lib.rs
+++ src/lib.rs
@@ -1,79 +1,74 @@
 use config::{builder::DefaultState, *};
 use std::collections::HashMap;
-use std::{path::PathBuf, fmt::Debug, error::Error};
+use std::{error::Error, fmt::Debug, path::PathBuf};
 
 #[derive(Debug, Clone)]
-pub struct Cfg {
+pub struct CfgBuilder {
     name: String,
     extension: String,
-    config: Config,
     path_override: Option<Vec<PathBuf>>,
+    defaults: Option<HashMap<String, Value>>,
 }
 
-pub enum ValueType {
-    String,
-    Int,
-    Float,
-    Bool,
-    Table,
-    Array,
-}
-
-impl Cfg {
-    pub fn from_config(name: &str, suffix: &str) -> Result<Self, ConfigError> {
-        let placeholder = Config::builder().build().expect("Should be able to construct empty config object");
-        let mut cfg = Self {
-            name: name.to_owned(),
-            extension: suffix.to_owned(),
-            config: placeholder,
-            path_override: None,
-        };
-        cfg.read_config()?;
-        Ok(cfg)
+impl CfgBuilder {
+    pub fn override_paths(&mut self, paths: Vec<PathBuf>) {
+        // todo: check input
+        self.path_override = Some(paths);
     }
 
-    pub fn read_config(&mut self) -> Result<(),ConfigError> {
-        let mut builder: ConfigBuilder<DefaultState> = Config::builder();
+    pub fn from_defaults(&mut self, defaults: HashMap<String, Value>) {
+        // todo: check input
+        self.defaults = Some(defaults);
+    }
+
+    pub fn build_config(self) -> Result<Config, ConfigError> {
         let paths = match self.path_override {
             Some(_) => self.path_override.clone().unwrap(),
             None => get_default_dirs(),
         };
-        let dropin_paths = paths.clone();
-        let configfile = find_conf(paths, &self.name, &self.extension);
-    
-        if configfile.is_some() {
-            builder = builder.add_source(File::from(configfile.unwrap()));
+
+        let mut builder = read_config(&self.name, &self.extension, paths)?;
+
+        if self.defaults.is_some() {
+            for (key, value) in self.defaults.unwrap() {
+                builder = builder.set_default(key, value)?;
+            }
         }
-    
-        let dropin_files = find_dropins(dropin_paths, &self.name);
-        let dropins = read_dropins(dropin_files)?;
-    
-        for (key, val) in dropins {
-            builder = builder.set_override(key, val)?;
-        }
-        self.config = builder.build()?;
-
-        Ok(())
+        builder.build()
     }
-
-    pub fn override_paths(&mut self, paths: Vec<PathBuf>) {
-        self.path_override = Some(paths);
-    }
-
-    pub fn get_value(&self, key: &str, rettype: ValueType) -> Result<ValueKind, ConfigError> {
-        let val = match rettype {
-            ValueType::String => ValueKind::String(self.config.get_string(key)?),
-            ValueType::Int => ValueKind::I64(self.config.get_int(key)?),
-            ValueType::Float => ValueKind::Float(self.config.get_float(key)?),
-            ValueType::Bool => ValueKind::Boolean(self.config.get_bool(key)?),
-            ValueType::Table => ValueKind::Table(self.config.get_table(key)?),
-            ValueType::Array => ValueKind::Array(self.config.get_array(key)?),
-        };
-        Ok(val)
-    }
-
 }
 
+pub fn new(name: &str, extension: &str) -> CfgBuilder {
+    CfgBuilder {
+        name: name.to_owned(),
+        extension: extension.to_owned(),
+        path_override: None,
+        defaults: None,
+    }
+}
+
+fn read_config(
+    name: &str,
+    extension: &str,
+    paths: Vec<PathBuf>,
+) -> Result<ConfigBuilder<DefaultState>, ConfigError> {
+    let mut builder: ConfigBuilder<DefaultState> = Config::builder();
+    let dropin_paths = paths.clone();
+    let configfile = find_conf(paths, name, extension);
+
+    if configfile.is_some() {
+        builder = builder.add_source(File::from(configfile.unwrap()));
+    }
+
+    let dropin_files = find_dropins(dropin_paths, name);
+    let dropins = read_dropins(dropin_files)?;
+
+    for (key, val) in dropins {
+        builder = builder.set_override(key, val)?;
+    }
+
+    Ok(builder)
+}
 
 fn get_default_dirs() -> Vec<PathBuf> {
     let etc_dir = PathBuf::from("/etc/");
@@ -82,13 +77,7 @@ fn get_default_dirs() -> Vec<PathBuf> {
     let usr_sharedir = PathBuf::from("/usr/share/");
     let usr_libdir = PathBuf::from("/usr/lib/");
 
-    vec![
-        etc_dir,
-        run_dir,
-        usr_etcdir,
-        usr_sharedir,
-        usr_libdir,
-    ]
+    vec![etc_dir, run_dir, usr_etcdir, usr_sharedir, usr_libdir]
 }
 
 fn find_conf(mut paths: Vec<PathBuf>, name: &str, suffix: &str) -> Option<PathBuf> {
@@ -130,12 +119,16 @@ fn find_dropins(conf_dirs: Vec<PathBuf>, name: &str) -> Vec<PathBuf> {
             dropin_paths.push(subconfd);
         }
     }
-    
+
     let mut dropin_list: Vec<PathBuf> = vec![];
     for path in dropin_paths {
         for entry in path.read_dir().expect("failed to read dir") {
             if let Ok(entry) = entry {
-                if entry.file_type().expect("dropin should have file type").is_file() {
+                if entry
+                    .file_type()
+                    .expect("dropin should have file type")
+                    .is_file()
+                {
                     dropin_list.push(entry.path());
                 }
             }
@@ -154,42 +147,18 @@ fn read_dropins(dropins: Vec<PathBuf>) -> Result<HashMap<String, Value>, ConfigE
         };
         // try to parse any unknown file format as ini
         let f = match ext {
-            "toml" | "json" | "yaml" | "yml" | "ini" | "ron" | "json5" => File::from(d).collect()?,
-            _ => { 
+            "toml" | "json" | "yaml" | "yml" | "ini" | "ron" | "json5" => {
+                File::from(d).collect()?
+            }
+            _ => {
                 log::debug!("Unknown file format. Trying to parse as ini");
                 File::new(d.to_str().unwrap(), FileFormat::Ini).collect()?
-            },
+            }
         };
         for (key, val) in f.iter() {
             dropin_cache.insert(key.clone(), val.clone());
         }
     }
-    
+
     Ok(dropin_cache)
 }
-/*
-pub fn merge_config(file1: Config::Config, file2: Config::Config) {
-
-}
-
-pub fn write_file(config: Config::Config, format: Config::FileFormat, path: PathBuf) -> Result<(), Error>{
-
-}
-
-pub fn set_value(confg: Config, key: &str {
-
-}
-
-pub fn get_value(config: Config, key: &str) {
-
-}
-
-pub fn add_file_format(name: &str, delim: &str, comment: &str) -> Config::ConfigError{
-
-}
-
-pub fn set_defaults(defaults: HashMap) {
-
-}
-
-*/
